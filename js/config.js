a076d29330c736c8e4a22fddfaf21be81cff3238
R Hari Setiawan <rhari.setiawan@gmail.com>
2026-07-04 20:34:06 +0700

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -10,7 +10,8 @@
 const APP_CONFIG = {
   // ── WAJIB DIISI setelah deploy Google Apps Script ──
   // Dapatkan URL dari: Apps Script → Deploy → Manage Deployments
-  API_URL: 'https://script.google.com/macros/s/AKfycbz20Df5FlXziAbfVbTPmiNx4d-LSju7gYNV_93KWhhRWSNYHy6Pe00B-eum9pc0erT4Dg/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwbyAmb5ckFlydwJs34DcfbI21gvv9vzdtI5SFFd5n6CMkKeDnvJIPk81EzzERuvnZ98w/exec',
+
 
   // ── Informasi Sekolah ──
   SCHOOL_NAME:    'MTS Al Huda Putri Malang',
